97d7fd4ab8fd230f71e11bc7455c300af849dd55
Eduardo <eduardomatoss004@gmail.com>
2021-05-09 15:23:48 -0300

    Adding aiohttp usage example

--- app/api.py
+++ app/api.py
@@ -3,11 +3,12 @@ from fastapi import FastAPI
 from app.routers import (
     root,
     health_check,
+    endpoint
 )
 from app.common.api_config import startup_aiohttp, shutdown_aiohttp
 
 APP_NAME = "fastapi-aiohttp-simple-example"
-APP_DESCRIPTION = "This is an example project of using fastapi with aiohttp"
+APP_DESCRIPTION = "This is an example of a fastapi project with aiohttp"
 API_RELEASE_VERSION = "1.0.0"
 
 app = FastAPI(
@@ -21,3 +22,4 @@ app = FastAPI(
 
 app.include_router(root.router)
 app.include_router(health_check.router)
+app.include_router(endpoint.router)
